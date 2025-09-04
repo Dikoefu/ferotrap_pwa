f9b336e5789ab0f63f490a6ca570edf2005b91cc
Dikoefu <105821122+Dikoefu@users.noreply.github.com>
2025-09-04 11:31:28 +0700

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,29 @@
+const CACHE_NAME = 'ferotrap-cache-v3';
+const urlsToCache = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json',
+  './icons/icon-192.png',
+  './icons/icon-512.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      if (response) return response;
+      return fetch(event.request).catch(() => {
+        if (event.request.headers.get('accept') && event.request.headers.get('accept').includes('text/html')) {
+          return caches.match('./index.html');
+        }
+      });
+    })
+  );
+});
